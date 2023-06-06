947da9721c2f509b4e2754826848fb625cfc7624
dharmik-tank-84 <dharmiktank84@gmail.com>
2023-06-06 15:03:34 +0530

    youtube-clone-app

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote'
+import HomeIcon from '@mui/icons-material/Home'
+import CodeIcon from '@mui/icons-material/Code'
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo'
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports'
+import LiveTvIcon from '@mui/icons-material/LiveTv'
+import SchoolIcon from '@mui/icons-material/School'
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural'
+import CheckroomIcon from '@mui/icons-material/Checkroom'
+import GraphicEqIcon from '@mui/icons-material/GraphicEq'
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy'
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode'
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+
+export const categories = [
+    { name: 'New', icons: <HomeIcon /> },
+    { name: 'JS Mastery', icons: <CodeIcon /> },
+    { name: 'Coding', icons: <CodeIcon /> },
+    { name: 'ReactJS', icons: <CodeIcon /> },
+    { name: 'NextJS', icons: <CodeIcon /> },
+    { name: 'Music', icons: <MusicNoteIcon /> },
+    { name: 'Education', icons: <SchoolIcon /> },
+    { name: 'Podcast', icons: <GraphicEqIcon /> },
+    { name: 'Movie', icons: <OndemandVideoIcon /> },
+    { name: 'Gaming', icons: <SportsEsportsIcon /> },
+    { name: 'Live', icons: <LiveTvIcon /> },
+    { name: 'Sport', icons: <FitnessCenterIcon /> },
+    { name: "Fashon", icons: <CheckroomIcon /> },
+    { name: 'Beauty', icons: <FaceRetouchingNaturalIcon /> },
+    { name: 'Comedy', icons: <TheaterComedyIcon /> },
+    { name: 'Gym', icons: <FitnessCenterIcon /> },
+    { name: 'Crypto', icons: <DeveloperModeIcon /> }
+];
+
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'Javascript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 Javascript & React API Projects in 10 Hours - Full Course | RapodAPI'
